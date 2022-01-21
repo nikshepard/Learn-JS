78a75516126ab36d1e68508b0ad7325dfa689dff
nikshepard <n7.nikshepard@gmail.com>
2022-01-21 19:41:10 +0300

    Update chapters 1&2, complete chapther 3

--- 1-Basics/Lesson-4.js
+++ 1-Basics/Lesson-4.js
@@ -1,4 +1,9 @@
-// Lesson-4 Checking exercises on the website
+//  Lesson-4 Checking exercises on the website
 
-// В этом задании объясняли как именно проверяются задания на сайте https://ru.code-basics.com/languages/javascript
+/*
+    В этом задании объясняли как именно проверяются задания на сайте https://ru.code-basics.com/languages/javascript
+*/
+
+//  Задание - Выведите на экран число 9780262531962
+
 console.log('9780262531962');
--- 1-Basics/Lesson-5.js
+++ 1-Basics/Lesson-5.js
@@ -1,7 +1,14 @@
-// Lesson-5 Syntax error
+//  Lesson-5 Syntax error
 
-// Попробуем запустить код, написанный ниже.
-//console.log('Hodor 
+/*
+    Попробуем запустить код, написанный ниже.
 
-//Если запустить код выше, то мы увидим следующее сообщение: SyntaxError: missing ) after argument list, а так же указание на строку и файл, где возникла эта ошибка. 
-//Подобные синтаксические ошибки в JavaScript относятся к разряду SyntaxError.
+    console.log('Hodor 
+
+    Если запустить код выше, то мы увидим следующее сообщение: SyntaxError: missing ) after argument list, а так же указание на строку и файл, где возникла эта ошибка. 
+    Подобные синтаксические ошибки в JavaScript относятся к разряду SyntaxError.
+*/
+
+//  Задание - Выведите на экран What Is Dead May Never Die
+
+console.log('What Is Dead May Never Die');
